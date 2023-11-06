2a2df73e17ae55cc9808726899bf37bbf882df25
minseo jang <mingdo.dev@gmail.com>
2023-11-06 18:05:28 +0900

    플랫리스트 컴포넌트 스타일이 이게 맞나요?

--- src/screens/DetailScreen.js
+++ src/screens/DetailScreen.js
@@ -4,11 +4,62 @@ import Header from '../components/Header/Header'
 import LottoNumberBox from '../components/LottoNumberBox'
 import { TextInput } from 'react-native';
 import SaveBtn from '../components/SaveBtn';
+import { FlatList } from 'react-native';
+import { Typography } from '../components/Typography';
+
+const data = [{
+    title: '내가 만약 로또에 당첨된다면 ...',
+    text: '좋겠다 여기에 적는 게 의미가 있을까? 였나? 부자되세요 만수무강 무병장수하세요',
+    id: 1,
+},
+{
+    title: '204902940293원',
+    text: '히히',
+    id: 2,
+},
+{
+    title: '204902940293원',
+    text: '히히',
+    id: 3,
+},
+{
+    title: '204902940293원',
+    text: '히히',
+    id: 4,
+},
+{
+    title: '204902940293원',
+    text: '히히',
+    id: 5,
+},
+{
+    title: '204902940293원',
+    text: '히히',
+    id: 6,
+},
+{
+    title: '204902940293원',
+    text: '히히',
+    id: 7,
+},
+]
 
 export default function DetailScreen({route}) {
     const [title, onChangeTitle] = useState('');
     const [text, onChangeText] = useState('');
 
+    const renderItem = ({item}) => {
+        return (
+            <View style={{ paddingVertical: 10 }}>
+                <View style={{ paddingBottom: 5 }}>
+                 <Typography fontSize={14} color='#007aff'>{item.title}</Typography>
+                </View>
+                <View>
+                 <Typography fontSize={13} color='#000000'>{item.text}</Typography>
+                </View>
+            </View>   
+        )
+    }
   return (
     <View style={{ flex: 1 }}>
 
@@ -45,6 +96,19 @@ export default function DetailScreen({route}) {
             </View>
         </View>
       </View>
+      <FlatList
+                data={data}
+                // data={[{title: title, text: text}]}
+                renderItem={renderItem}
+                keyExtractor={(item)=>String(item.id)}
+                contentContainerStyle={{
+                    backgroundColor:'#d9d9d9',
+                    paddingHorizontal: 20,
+                    paddingVertical: 10,
+                    marginHorizontal: 24
+                }}
+                // style={{ paddingHorizontal: 20, height: 350 }}
+            />
       
     </View>
   )
